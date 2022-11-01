9c9669f9c3e0b3df4d4804a6848411f23a1c5b54
ericVlz <valenz_eric@hotmail.com>
2022-11-01 11:34:06 -0700

    login
    se añadio el login y otros avances de mi parte

--- RUIM/Aplicacion/models.py
+++ RUIM/Aplicacion/models.py
@@ -11,5 +11,17 @@ class InputModel(models.Model):
     tipo = models.CharField(max_length=250, choices=[('Platica', 'platica'), ('Poster', 'poster')], default='')
     resumen = models.FileField(upload_to='resumenes/%Y', null=True)
 
+    ESTADO_REVISION = [
+        (1, 'sin revision'),
+        (2, 'pendiente'),
+        (3, 'aceptada'),
+        (4, 'rechazada'),
+    ]
+    
+    estado = models.IntegerField(
+        choices=ESTADO_REVISION,
+        default=1,
+    )
+    
     def __str__(self):
         return self.correo
--- RUIM/RUIM/urls.py
+++ RUIM/RUIM/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from Aplicacion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('',include('Aplicacion.urls')),
     #path('', views.home),
-    path('formularioRegistro/', views.formularioRegistro),
-    path('guardar/', views.guardar)
+    
 ]
