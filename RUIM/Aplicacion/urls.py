f643a8b17b22e2ce18e8762fdcbdffaa52d4cde7
sergioadair <sergioadairlopez@gmail.com>
2022-11-02 13:28:57 -0700

    Se arregló el problema del tipo de archivo docx

--- RUIM/Aplicacion/urls.py
+++ RUIM/Aplicacion/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
     path('formularioRegistro/', views.formularioRegistro),
-    path('guardar/', views.guardar),
+    path('guardarFR/', views.guardarFR),
     
     path('login/listado', views.listado, name='listado'),
     path('login/seleccion', views.seleccionPonencias, name='seleccion'),
--- RUIM/Aplicacion/views.py
+++ RUIM/Aplicacion/views.py
@@ -5,28 +5,21 @@ from django.contrib.auth.decorators import login_required
 from .forms import InputForm
 from .models import InputModel
 
-import magic
 
 # Create your views here.
 def home(request):
     return HttpResponse("Home :)")
 
-"""def home(request):
-    context = {}
-    context['form'] = InputForm()
-    return render(request, "formularioRegistro.html", context)"""
 
 def formularioRegistro(request):
     context = {}
     context['form'] = InputForm()
     return render(request, "formularioRegistro.html", context)
 
-def guardar(request):
+def guardarFR(request):
     if request.method=='POST' and request.FILES['resumen']:
         form = InputForm(request.POST, request.FILES)
-        # Esto es para verificar que el tipo de archivo sea docx
-        tipoArchivo = magic.from_buffer(request.FILES['resumen'].read(), mime=True)
-        if form.is_valid() and tipoArchivo=='application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        if form.is_valid() and request.FILES['resumen'].name.endswith('.docx'):
             form.save()
         else:
             raise ValidationError(u'Error de validación')
