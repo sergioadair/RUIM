54438e3181e4835d70849dba94b52b72b7ebcbcc
ericVlz <valenz_eric@hotmail.com>
2022-11-03 13:48:39 -0700

    estilos2

--- RUIM/RUIM/settings.py
+++ RUIM/RUIM/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Aplicacion',
+    'crispy_forms',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+    
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
